abc1398bbfa5b9a06c614656e2a2bb0ac6be7107
Itzel Cruz <Itzelyajairacruzhernandez7@gmail.com>
2025-01-27 12:38:57 -0600

    Subir archivos iniciales

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Ejemplo01 } from './components/Ejemplos/Ejemplo01';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola</h1>
+       <Ejemplo01/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/Home/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function home() {
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
+
