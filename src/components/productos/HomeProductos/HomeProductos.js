d0dcff5858e2ab970a996d7f073ed9a0bc46c2ce
Itzel Cruz <Itzelyajairacruzhernandez7@gmail.com>
2025-02-06 13:25:49 -0600

    card bootstrap

--- src/components/productos/HomeProductos/HomeProductos.js
+++ src/components/productos/HomeProductos/HomeProductos.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import {Tabs, Tab} from "react-bootstrap";
-import {ItemProductos} from "../ItemProductos";
-import {Datos} from '../../../utils/bd';
+import React from "react";
+import { Tabs, Tab } from "react-bootstrap";
+import { ItemProductos } from "../ItemProductos";
+import { Datos } from "../../../utils/bd";
 
 export function HomeProductos() {
-console.log(Datos);
+  console.log(Datos);
 
-  const fondo={
-    tema:{
-      backgroundColor:'floralwhite',
-      color:"palevioletred",
-      fontSize:'20px'
-    }
-  }
+  const fondo = {
+    tema: {
+      backgroundColor: "floralwhite",
+      color: "palevioletred",
+      fontSize: "20px",
+    },
+  };
   return (
-    <div className='container' style={fondo.tema}>
-    <Tabs
-    defaultActiveKey="profile"
-    id="uncontrolled-tab-example"
-    className="mb-3"
-  >
-    <Tab eventKey="home" title="Lista de Productos">
-      <ItemProductos datos={Datos}/>
-    </Tab>
-  </Tabs>
-  </div>
-  )
+    <div className="container" style={fondo.tema}>
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Lista de Productos">
+          <ItemProductos datos={Datos} />
+        </Tab>
+      </Tabs>
+
+      {/* <div eventKey="home" title="Lista de Productos">
+        <ItemProductos datos={Datos} />
+      </div> */}
+    </div>
+  );
 }
 
-export default HomeProductos
+export default HomeProductos;
--- src/components/productos/ItemProductos/ItemProductos.js
+++ src/components/productos/ItemProductos/ItemProductos.js
@@ -1,37 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export function ItemProductos({datos}) {
   return (
-    <div>
-      <h2>Lista de Productos</h2>
-      <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-        {datos.map((producto) => (
-          <div key={producto.id} className="card" style={{ width: "18rem" }}>
-            <img
-              src={producto.imagen}
-              className="card-img-top"
-              alt={producto.nombre}
-            />
-            <div className="card-body">
-              <h5 className="card-title">{producto.nombre}</h5>
+      <div className="container">
+        <h2>Lista de Productos</h2>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          {datos.map((producto) => (
+            <div key={producto.id} className="col">
+              <Card style={{ width: '18rem' }} className="h-100">
+                <Card.Img variant="top" src={producto.imagen} alt={producto.nombre} />
+                <Card.Body>
+                  <Card.Title>{producto.nombre}</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>Precio: ${producto.precio}</ListGroup.Item>
+                  <ListGroup.Item>Cantidad Disponible: {producto.cantidad}</ListGroup.Item>
+                  <ListGroup.Item>Unidad: {producto.unidad}</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                  <Link to={`/detalle/${producto.id}`} className="card-link">
+                    Ver Detalles
+                  </Link>
+                </Card.Body>
+              </Card>
             </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">Precio: ${producto.precio}</li>
-              <li className="list-group-item">
-                Cantidad Disponible: {producto.cantidad}
-              </li>
-              <li className="list-group-item">Unidad: {producto.unidad}</li>
-            </ul>
-            <div className="card-body">
-              <Link to={`/detalle/${producto.id}`} className="card-link">
-                Ver Detalles
-              </Link>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
   );
 }
 
